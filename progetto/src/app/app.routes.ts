c3d4337368847bb5b30f17eca139244acb4ea240
Emtecsus <flaviodipietro731@gmail.com>
2025-05-20 00:10:21 +0200

    Login e Signup

--- progetto/src/app/app.routes.ts
+++ progetto/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
 
 export const routes: Routes = [
   {
--- progetto/src/app/signup/signup.page.ts
+++ progetto/src/app/signup/signup.page.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonCol, IonContent, IonGrid, IonInput, IonItem, IonLabel, IonRow } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, 
+    CommonModule, 
+    FormsModule,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonCol,
+    IonRow,
+    IonGrid]
 })
 export class SignupPage implements OnInit {
 
