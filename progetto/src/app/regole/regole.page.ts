e39cba85c987877684176b6e7d698cc4c7b7c8e6
Reversiano <reversianotiklappa@gmail.com>
2025-05-20 00:45:47 +0200

    upgrade di regole.page.html e aggiunta di IonicModule in ts

--- progetto/src/app/regole/regole.page.ts
+++ progetto/src/app/regole/regole.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-regole',
   templateUrl: './regole.page.html',
   styleUrls: ['./regole.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule]
 })
 export class RegolePage implements OnInit {
 
