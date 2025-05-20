e40503f87c2143a14f79b4cbf879095b045c0a95
Reversiano <reversianotiklappa@gmail.com>
2025-05-20 22:28:40 +0200

    rules update

--- progetto/src/app/rules/rules.page.ts
+++ progetto/src/app/rules/rules.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -13,9 +12,22 @@ import { IonicModule } from '@ionic/angular';
 })
 export class RulesPage implements OnInit {
 
+  isLoading: boolean = true;
+
+  rules = [
+    { title: 'Regola 1', description: 'Descrizione della regola 1.' },
+    { title: 'Regola 2', description: 'Descrizione della regola 2.' },
+    { title: 'Regola 3', description: 'Descrizione della regola 3.' },
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    // Simula un caricamento di 2 secondi prima di mostrare le regole
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
   }
 
 }
+
