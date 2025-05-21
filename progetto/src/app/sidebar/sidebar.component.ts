578d81ec11dd209b1dec6aa6ccf0ea6f9504ff1d
LHODURO90 <daniele2003c@gmail.com>
2025-05-21 14:04:09 +0200

    Aggiunto percorso rules in sidebar

--- progetto/src/app/sidebar/sidebar.component.ts
+++ progetto/src/app/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,16 +11,22 @@ import { Router } from '@angular/router';
   imports:[IonicModule],
 })
 export class SidebarComponent {
+    @Input() menuId = '';
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private menu:MenuController
   ) {}
 
   onLogout() {
     this.authService.logout();
     this.router.navigateByUrl('/login');
     
+  }
+  onRules(){
+    this.menu.close(this.menuId);
+    this.router.navigateByUrl('/rules');
   }
 
 }
